43d63c24e10b8ae6f55a49e2dbf01b00313e8587
Fiofi Fiorito <fiofi.fiorito@gmail.com>
2023-07-24 19:42:05 -0300

    try to add map func to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,30 @@ import { Operator, ResetButton } from './components/button';
 import Screen from './components/screen';
 import '../public/styles.css';
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operators = ["+", "-", "x", "÷"];
+
+function numButton() {
+  numbers.forEach(() => {
+    return <Button character={numbers.number} />
+  })
+};
+
+function opButton() {
+  operators.forEach(() => {
+    return <Operator character={operators.operator} />
+  })
+
+};
+
 
 function App() {
   return <div className='layout-div'>
     <Screen result="Here" />
     <div className='calculator-div'>
-      <Button character="1" />
-      <Button character="2" />
-      <Button character="3" />
-      <Operator character="+" />
-      <Button character="4" />
-      <Button character="5" />
-      <Button character="6" />
-      <Operator character="-" />
-      <Button character="7" />
-      <Button character="8" />
-      <Button character="9" />
-      <Operator character="x" />
-      <ResetButton character="C" />
-      <Button character="0" />
-      <Button character="," />
-      <Operator character="÷" />
+      {numbers.map(numButton)}
+      {operators.map(opButton)}
+
     </div>
   </div>
 }
