25e3436d289a0974895d637a1d323610f950fb6b
Fiofi Fiorito <fiofi.fiorito@gmail.com>
2023-07-31 19:20:42 -0300

    add hidden input value to buttons

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,19 +1,19 @@
 
 function Button(props) {
     return <div className="num-button">
-        <p>{props.character}</p>
+        <p>{props.character} <input type="hidden" value={props.character}></input></p>
     </div>
 }
 
 function Operator(props) {
     return <div className="operator-button">
-        <p>{props.character}</p>
+        <p>{props.character} <input type="hidden" value={props.character}></input></p>
     </div>
 }
 
 function ResetButton(props) {
     return <div className="reset-button">
-        <p>{props.character}</p>
+        <p>{props.character} <input type="hidden" value={props.character}></input></p>
     </div>
 }
 
